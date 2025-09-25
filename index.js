9248659829292efa1093f152593b17decc693890
sshankar@uticorp.com <sshankar@uticorp.com>
2025-09-25 11:55:27 +0530

    test

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Node.js!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
